0abc8953d2d61f6f4a83236e5e36c1747bb68211
WTNB1225 <w.swallowsfan@gmail.com>
2024-02-07 13:01:24 +0900

    finish test

---
+++ adder/target/debug/deps/adder-06f588c871c2de64.d
@@ -0,0 +1,5 @@
+/Users/watanabeyuki/rust_practice/adder/target/debug/deps/adder-06f588c871c2de64: src/lib.rs
+
+/Users/watanabeyuki/rust_practice/adder/target/debug/deps/adder-06f588c871c2de64.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/integration_test-24e911434da68828.d
@@ -0,0 +1,6 @@
+/Users/watanabeyuki/rust_practice/adder/target/debug/deps/integration_test-24e911434da68828: tests/integration_test.rs tests/common.rs
+
+/Users/watanabeyuki/rust_practice/adder/target/debug/deps/integration_test-24e911434da68828.d: tests/integration_test.rs tests/common.rs
+
+tests/integration_test.rs:
+tests/common.rs:
